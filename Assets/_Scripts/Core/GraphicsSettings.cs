b1059de8c6eae55433fb9d0169feb101afa93da2
Johann <johannneudeck@gmx.de>
2025-01-14 14:02:33 +0100

    Fixed broken Lights

---
+++ Assets/_Scripts/Core/GraphicsSettings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DynamicGI.UpdateEnvironment();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
