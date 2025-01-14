c44cf412c47946a79b87eb8b1deee46f72322116
Johann <johannneudeck@gmx.de>
2025-01-14 14:29:48 +0100

    Did  the thing

---
+++ Assets/_Scripts/Game/GameSetup/DogInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogInfo : MonoBehaviour
+{
+    Dictionary<int, string> dogSkins = new Dictionary<int, string>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
