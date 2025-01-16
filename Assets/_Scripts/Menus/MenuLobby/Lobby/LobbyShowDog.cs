bab83cf213a6c164673a23dce9aad1655c81f1d4
XenonnoneX <xeno.boecker@web.de>
2025-01-16 16:06:14 +0100

    final fix lobby dog selector

--- Assets/_Scripts/Menus/MenuLobby/Lobby/LobbyShowDog.cs
+++ Assets/_Scripts/Menus/MenuLobby/Lobby/LobbyShowDog.cs
@@ -67,7 +67,6 @@ public class LobbyShowDog : MonoBehaviour
         }
         else
         {
-            selectAccessoriePanel.SetActive(true);
             showConfirmed.SetActive(false);
         }
     }
