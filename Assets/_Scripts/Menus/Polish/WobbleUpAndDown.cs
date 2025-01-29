1939ebf76b44b8eee5d130b88701fce77ea8c8d3
XenonnoneX <xeno.boecker@web.de>
2025-01-29 14:19:50 +0100

    start screen wobble, score vfx updated, credits scrolling, dog ingame shadow, timeLeft Text fixed

---
+++ Assets/_Scripts/Menus/Polish/WobbleUpAndDown.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class WobbleUpAndDown : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f, moveScale = 0.5f;
+    [SerializeField] private float rotSpeed = 2f, rotScale = 15f;
+
+    private float timeOffset;
+
+    private void Start()
+    {
+        // Offset to ensure different objects wobble at different starting points
+        timeOffset = Random.Range(0f, Mathf.PI * 2);
+    }
+
+    private void Update()
+    {
+        float wobble = Mathf.Sin(Time.time * moveSpeed + timeOffset);
+
+        // Move up and down
+        transform.localPosition = new Vector3(transform.localPosition.x, wobble * moveScale, transform.localPosition.z);
+
+        // Rotate left and right
+        transform.localRotation = Quaternion.Euler(0, 0, wobble * rotScale);
+    }
+}
