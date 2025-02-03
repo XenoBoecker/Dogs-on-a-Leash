4a625ac47acbd3d42a315f7c66d37b65d7a9a5b0
XenonnoneX <xeno.boecker@web.de>
2025-02-03 12:51:58 +0100

    The ball will return!

--- Assets/_Scripts/Game/Ball.cs
+++ Assets/_Scripts/Game/Ball.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    HumanMovement human;
+
     float goalDist;
 
     bool done;
+
+
+    [SerializeField] float respawnActivateDist = 30, respawnForwardDist = 30, minRestPathLength = 50;
+
+    [SerializeField] int maxRespawnCount;
+    int respawnCount;
+
     // Start is called before the first frame update
     void Start()
     {
+        human = FindObjectOfType<HumanMovement>();
+
         goalDist = FindObjectOfType<MapManager>().TotalPathLength;
     }
 
@@ -17,6 +28,23 @@ public class Ball : MonoBehaviour
     void Update()
     {
         if (done) return;
+
+        if(human.transform.position.x - transform.position.x > respawnActivateDist)
+        {
+            Debug.Log("RespawnDist");
+            if(human.transform.position.x + respawnForwardDist + minRestPathLength < goalDist)
+            {
+                Debug.Log("still enough space");
+                if(respawnCount < maxRespawnCount)
+                {
+                    Debug.Log("respawn");
+                    GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    transform.position = new Vector3(human.transform.position.x + respawnForwardDist, 1f, 0f);
+
+                    respawnCount++;
+                }
+            }
+        }
 
         if (transform.position.x > goalDist)
         {
