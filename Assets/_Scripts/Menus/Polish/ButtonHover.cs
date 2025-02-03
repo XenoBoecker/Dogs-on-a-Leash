61c518e65d7bbd951933a616c01f9d90d172b318
XenonnoneX <xeno.boecker@web.de>
2025-02-03 21:49:22 +0100

    EventSystem button hover errors fixed

--- Assets/_Scripts/Menus/Polish/ButtonHover.cs
+++ Assets/_Scripts/Menus/Polish/ButtonHover.cs
@@ -8,6 +8,10 @@ public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     [SerializeField] bool playSoundOnHover = true;
     [SerializeField] float audioPitchMin = 0.9f, audioPitchMax = 1.1f;
 
+    static bool isSelecting;
+
+    static Coroutine setSelectedCoroutine;
+
     protected virtual void Start()
     {
 
@@ -25,14 +29,14 @@ public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 
     public virtual void OnHoverEnter()
     {
-        EventSystem.current.SetSelectedGameObject(gameObject);
-
-        if(playSoundOnHover) SoundManager.Instance.PlaySoundWithRandomPitch(SoundManager.Instance.uiSFX.buttonHoverSound, null, audioPitchMin, audioPitchMax);
+        // if (setSelectedCoroutine != null) StopCoroutine(setSelectedCoroutine); // does not work for some reason
+        StartCoroutine(SetSelectedAfterFrame());
+        if (playSoundOnHover) SoundManager.Instance.PlaySoundWithRandomPitch(SoundManager.Instance.uiSFX.buttonHoverSound, null, audioPitchMin, audioPitchMax);
     }
 
     public virtual void OnHoverExit()
     {
-        EventSystem.current.SetSelectedGameObject(null);
+        
     }
 
     public void OnSelect(BaseEventData eventData)
@@ -47,6 +51,16 @@ public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 
     public void OnButtonClick()
     {
+        Debug.Log("OnButtonClick");
         SoundManager.Instance.PlaySound(SoundManager.Instance.uiSFX.buttonClickSound);
+    }
+    private System.Collections.IEnumerator SetSelectedAfterFrame()
+    {
+        if (isSelecting) yield break;
+
+        isSelecting = true;
+        yield return null; // Wait until the end of the frame
+        EventSystem.current.SetSelectedGameObject(gameObject);
+        isSelecting = false;
     }
 }
