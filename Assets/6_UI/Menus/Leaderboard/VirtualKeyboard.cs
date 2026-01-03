64c01c0ad8e64145bbe04d298fb75d2342fe6cfc
XenoBoecker <xeno.boecker@web.de>
2026-01-03 14:29:13 +0100

    virtual keyboard for leaderboard prepared

---
+++ Assets/6_UI/Menus/Leaderboard/VirtualKeyboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class VirtualKeyboard : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInputField;
+
+    private bool isCapsLockOn = false;
+
+
+    public event Action<bool> OnCapsLockToggled;
+
+    public static VirtualKeyboard Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LetterEntered(string letter)
+    {
+        if (isCapsLockOn)
+        {
+            letter = letter.ToUpper();
+        }
+        nameInputField.text += letter;
+    }
+
+    public void BackspacePressed()
+    {
+        if (nameInputField.text.Length > 0)
+        {
+            nameInputField.text = nameInputField.text.Substring(0, nameInputField.text.Length - 1);
+        }
+    }
+
+    public void CapsLockToggled()
+    {
+        isCapsLockOn = !isCapsLockOn;
+
+        OnCapsLockToggled?.Invoke(isCapsLockOn);
+    }
+}
---
+++ Assets/6_UI/Menus/Leaderboard/VirtualKeyboardKey.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class VirtualKeyboardKey : MonoBehaviour
+{
+    [SerializeField] private string keyValue; // The value this key represents
+    [SerializeField] private TMP_Text keyText;
+
+    private void Start()
+    {
+        VirtualKeyboard.Instance.OnCapsLockToggled += HandleCapsLockToggled;
+
+        HandleCapsLockToggled(false);
+    }
+
+    public void OnKeyPressed()
+    {
+        VirtualKeyboard.Instance.LetterEntered(keyValue);
+    }
+
+    private void HandleCapsLockToggled(bool isCapsLocked)
+    {
+        keyText.text = isCapsLocked ? keyValue.ToUpper() : keyValue.ToLower();
+    }
+
+    private void OnDestroy()
+    {
+        VirtualKeyboard.Instance.OnCapsLockToggled -= HandleCapsLockToggled;
+    }
+}
