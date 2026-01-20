efa08f8ba37e7ac1599c748308534c23b1ced29b
XenoBoecker <xeno.boecker@web.de>
2026-01-20 12:56:39 +0100

    Toggle Tutorial in settings; new tutorial gifs

---
+++ Assets/6_UI/Menus/Settings/SettingsMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] Slider musicVolumeSlider;
+    [SerializeField] Slider sfxVolumeSlider;
+
+    [SerializeField] private Toggle tutorialToggle;
+
+
+    private void Start()
+    {
+        musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
+
+        tutorialToggle.isOn = PlayerPrefs.GetInt("ShowTutorial", 1) == 1;
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", value);
+
+        ReloadSoundManager();
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        PlayerPrefs.SetFloat("SFXVolume", value);
+
+        ReloadSoundManager();
+    }
+
+    void ReloadSoundManager()
+    {
+        if (SoundManager.Instance == null) return;
+
+        SoundManager.Instance.Reload();
+    }
+
+    public void SetShowTutorial(bool show)
+    {
+        PlayerPrefs.SetInt("ShowTutorial", show ? 1 : 0);
+    }
+}
