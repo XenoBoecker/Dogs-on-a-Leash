d705cb0f04b117ae0e748876256fb18b4b015750
XenoBoecker <xeno.boecker@web.de>
2026-08-20 18:26:38 +0200

    MainMenu text transparent hover

---
+++ Assets/6_UI/Menus/Commons/ButtonHoverTextColorChange.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ButtonHoverTextColorChange : MonoBehaviour
+{
+    [SerializeField] private ButtonHover buttonHover;
+    [SerializeField] private Color hoverColor;
+    [SerializeField] private Color notHoverColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (buttonHover == null) buttonHover = GetComponent<ButtonHover>();
+        if (buttonHover.IsHovering)
+        {
+            SetHovering();
+        }
+        else
+        {
+            SetNotHovering();
+        }
+        buttonHover.OnHoverEntered += SetHovering;
+        buttonHover.OnHoverExited += SetNotHovering;
+    }
+
+    void SetHovering()
+    {
+        GetComponent<TMP_Text>().color = hoverColor;
+    }
+
+    void SetNotHovering()
+    {
+        GetComponent<TMP_Text>().color = notHoverColor;
+    }
+}
