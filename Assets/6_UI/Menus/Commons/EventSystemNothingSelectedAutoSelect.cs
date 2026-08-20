15bbca9d6ed51914d83ad83cf7956a1772685c68
XenoBoecker <xeno.boecker@web.de>
2026-08-20 16:23:00 +0200

    EventSystemNothingSelectedAutoSelect

---
+++ Assets/6_UI/Menus/Commons/EventSystemNothingSelectedAutoSelect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemNothingSelectedAutoSelect : MonoBehaviour
+{
+    [Tooltip("If nothing is selected, EventSystem firstSelectedObject will be selected.")]
+    [SerializeField] private GameObject autoSelectObject;
+
+    private void Update()
+    {
+        if(EventSystem.current.currentSelectedGameObject == null)
+        {
+            if(autoSelectObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
+            }
+            else
+            {
+                EventSystem.current.SetSelectedGameObject(autoSelectObject);
+            }
+        }
+    }
+}
